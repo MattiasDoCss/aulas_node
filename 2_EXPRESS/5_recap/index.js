7a8a0404f1cc0c04f3e5365339e3a11a2985d181
EA - Mateus Fernandes Mano <mateus_mano@educadventista.org>
2023-09-25 14:49:34 -0300

    recaptulação de render html

--- 2_EXPRESS/5_recap/index.js
+++ 2_EXPRESS/5_recap/index.js
@@ -1,12 +1,15 @@
 const express = require("express")//Importa o express
+const path = require("path")
 
 const app = express()
 
+const caminho = path.join(__dirname, "templates")
+
 app.get('/', (requisicao, respostas) => {
-    respostas.send("Seja bem-vindo ao meu servidor!")
+    respostas.sendFile(`${caminho}/index.html`)
 })
 
-app.listen(3000, () => {
+app.listen(3001, () => {
     console.log("Servidor rodando na porta 3k")
 })
 
